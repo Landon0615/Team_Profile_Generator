768425971a32e9ea1fa92891c38a50b14efc0205
Landon Waddell <Landon.waddell@gmail.com>
2021-07-02 08:07:27 -0600

    Added folders for team, test and generated, added app.js.

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const Manager = require("./team/Manager");
+const Engineer = require("./team/Engineer");
+const Employee = require("./team/Employee")
+const Intern = require("./team/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
